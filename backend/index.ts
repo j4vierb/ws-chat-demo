9fbba881066850fa827b96e2f170f5737b6a9976
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-07-08 11:48:56 -0500

    Add native TypeScript exec with node

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,26 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import { randomUUID } from "crypto";
+import type { Message } from "./types";
 
 const wss = new WebSocketServer({ port: 8080 });
-const messages = [];
+const messages: Array<Message> = [];
 
 console.log("WebSocket server is running on ws://localhost:8080");
 
 wss.on("connection", (ws) => {
-  const clientId = randomUUID();
+  const clientId: string = randomUUID();
   console.log(`New client connected: "${clientId}"`);
 
   ws.on('error', console.error);
 
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    const newMessage = { message: message.toString(), client: clientId };
+    const newMessage: Message = { message: message.toString(), client: clientId };
     messages.push(newMessage);
 
     // Broadcast the message to all connected clients
-    console.log(`Broadcasting message to ${wss.clients.size} clients`);
-    wss.clients.forEach((client) => {
+    console.log(`Broadcasting message to ${wss.clients.size - 1} clients`);
+    wss.clients.forEach((client: WebSocket) => {
       if (client !== ws && client.readyState === client.OPEN) {
         client.send(JSON.stringify(newMessage));
       }
@@ -33,3 +34,4 @@ wss.on("connection", (ws) => {
   // Send the current messages to the newly connected clients
   ws.send(JSON.stringify({ messages: messages, client: clientId, first: true }));
 });
+
